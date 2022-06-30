8793c55f91fb051db8e2acd300193a339085110b
Junioromondi <omondi.junior@yahoo.com>
2022-06-30 04:40:17 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
-/*
- *
- *
- */
-int main(void)
+
+main()
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
+
 }
