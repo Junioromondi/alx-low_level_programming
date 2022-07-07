64c9d02f1152790335c79113f137b4e13eca84dc
Junioromondi <omondi.junior@yahoo.com>
2022-07-07 03:39:40 -0700

    supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
-#include "main.h"
-#include <stdio.h>
+#include "mainn.h"
 
 /**
- * _isupper - checks if parameter is an uppercase character.
- * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
- */
-int _isupper(int c)
+* _isdigit - checks if parameter is a number between 0 to 9.
+* @c: input number.
+* Return: 1 if is a number (0 to 9), 0 in other case.
+*/
+
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
