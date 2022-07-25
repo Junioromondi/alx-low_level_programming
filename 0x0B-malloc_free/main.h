94ef2af8a4f2b68f1662e499d32e5919d5a0945c
Junioromondi <omondi.junior@yahoo.com>
2022-07-25 14:03:29 -0700

    task0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,3 +3,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(usigned int nmeb, unsigned int size);
 int *array_range(int min, int max);
 void *realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *create_array(unsigned int size, char c);
