6a0fb58536eb6376ca7df08f483daaa81b326816
Junioromondi <omondi.junior@yahoo.com>
2022-08-12 02:55:02 -0700

    task0

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,5 +1,17 @@
 #ifndef _LISTS_H
 #define _LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 _putchar (char c);
 
