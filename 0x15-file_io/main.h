99e25c1597e853da8741673aa1ff7b274211893f
Junioromondi <omondi.junior@yahoo.com>
2022-08-23 06:59:48 -0700

    task0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
